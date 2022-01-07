852bebb9f58166e169e8c9d401196d2c2f35d5a1
Pc <Pc@DESKTOP-VOI8PG4>
2022-01-07 11:32:00 +0100

    Dodaj pliki projektów.

---
+++ Snake3/Input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+
+namespace Snake3
+{
+    internal class Input
+    {
+        
+        private static Hashtable keyTable = new Hashtable();
+
+        
+        public static bool KeyPressed(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool)keyTable[key];
+        }
+
+        
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+    }
+}
